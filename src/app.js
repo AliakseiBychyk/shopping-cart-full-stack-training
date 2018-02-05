0052e4dac88268b842b01faed1c7c7ab926a5d5d
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-02-05 16:16:52 +0300

    dispatch second action

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import { createStore } from 'redux'
 
 // STEP 3 define reducers
-const reducer = (state = [], action) => {
+const reducer = (state = { books: [] }, action) => {
   switch (action.type) {
     // case 'INCREMENT':
     //   return state + action.payload
     // case 'DECREMENT':
     //   return state - action.payload 
     case 'POST_BOOK':
-      return state = action.payload
+ 
+      return { books: [...state.books, action.payload] }
     break  
   }
   return state
@@ -20,27 +21,34 @@ const store = createStore(reducer,
 )
 store.subscribe(() => {
   console.log(`current state is`, store.getState())
-  console.log('current price is ', store.getState()[1].price )
+  console.log('current price is ', store.getState().books[0].price )
 })
 
 // STEP 2 create and dispatch actions
+
 // store.dispatch({ type: 'INCREMENT', payload: 1 })
 // store.dispatch({ type: 'INCREMENT', payload: 10 })
 // store.dispatch({ type: 'DECREMENT', payload: 1 })
 store.dispatch({
   type: 'POST_BOOK',
-  payload: [{
+  payload: {
     id: 1,
     title: 'this is the book title',
     description: 'this is the book description',
     price: 33.33
-  },
+  }
+})
+
+// DISPATCH a second action
+store.dispatch({
+  type: 'POST_BOOK',
+  payload: 
   {
     id: 2,
     title: 'this is the second book title',
     description: 'this is the book description',
     price: 45.25
-  }]
+  }
 })
 
 
